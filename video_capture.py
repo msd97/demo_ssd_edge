3ebb9e73a95124b093f0badcbddaa301e8d31476
msd97 <mariosilvestre@hotmail.es>
2021-07-16 10:40:31 -0500

    Added CSI camera parameters for Jetson Nano

--- video_capture.py
+++ video_capture.py
@@ -4,7 +4,38 @@ import tensorflow as tf
 import tensorflow_hub as hub
 from ssd_tf_hub import inference
 
-cap = cv2.VideoCapture(0)
+def gstreamer_pipeline(
+    sensor_id=0,
+    sensor_mode=3,
+    capture_width=1280,
+    capture_height=720,
+    display_width=1280,
+    display_height=720,
+    framerate=60,
+    flip_method=0,
+):
+    return (
+       "nvarguscamerasrc sensor-id=%d sensor-mode=%d ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            sensor_mode,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
 
 os.environ['TFHUB_CACHE_DIR'] = './hub_directory/tf_cache'
 
